95f030d6cd6b6a51f20a2ae9e679e62a35baeae5
linjianghe <linjianghe@tencent.com>
2019-07-22 21:02:13 +0800

    feat: 新增 simple 页面

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Help from './pages/help';
+import PageSimple from './pages/simple';
 import Index from './pages/index';
 import Document from './pages/document';
 
 import './App.less';
 
-export default class App extends Component {
-    constructor(...props) {
-        super(...props);
-    }
-
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path={`/`} component={Index} />
-                    <Route path={`/help`} component={Help} />
-                    <Route path={`/document`} component={Document} />
-                </Switch>
-            </Router>
-        );
-    }
+export default function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path={`/`} component={Index} />
+                <Route path={`/simple`} component={PageSimple} />
+                <Route path={`/document`} component={Document} />
+            </Switch>
+        </Router>
+    );
 }
