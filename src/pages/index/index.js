b713c63b8567df10245822f765ffaa30eea55f25
linjianghe <linjianghe@tencent.com>
2019-07-22 20:49:58 +0800

    feat: 引入 redux 和 路由等

---
+++ src/pages/index/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './index.less';
+
+export default class PageIndex extends Component {
+
+    render() {
+        return (
+            <div className="page-index">
+                <h2>hello,world!</h2>
+            </div>
+        );
+    }
+}
