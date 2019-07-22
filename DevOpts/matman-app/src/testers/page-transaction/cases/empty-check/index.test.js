a7719857fe467c329ffce849a2b2ffd944015d8b
helinjiang <onehlj@qq.com>
2019-07-22 23:08:26 +0800

    feat: 新增 matman-app

---
+++ DevOpts/matman-app/src/testers/page-transaction/cases/empty-check/index.test.js
@@ -0,0 +1,36 @@
+const expect = require('chai').expect;
+const checkPage = require('.');
+
+describe('transaction.html：无流水信息检查', function () {
+    this.timeout(30000);
+
+    let resultData;
+
+    before(function () {
+        return checkPage({ show: false, doNotEnd: false, useRecorder: false })
+            .then(function (result) {
+                // console.log(JSON.stringify(result));
+                resultData = result;
+            });
+    });
+
+    describe('检查基本信息', function () {
+        let data;
+
+        before(function () {
+            data = resultData.data;
+        });
+
+        it('数据快照校验通过', function () {
+            expect(data).to.eql({
+                'transactionListInfo': {
+                    'emptyPic': 'http://now.qq.com/img/nopkdata@2x_c3c9fbba.png',
+                    'emptyWording': '暂无流水记录',
+                    'isExist': true,
+                    'list': [],
+                    'total': 0
+                }
+            });
+        });
+    });
+});
