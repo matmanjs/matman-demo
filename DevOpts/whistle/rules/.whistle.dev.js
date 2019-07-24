15140794eb83763d64f5d4f44bb94faba882d870
helinjiang <onehlj@qq.com>
2019-07-24 19:57:14 +0800

    feat: 新增 whistle 配置

--- DevOpts/whistle/rules/.whistle.dev.js
+++ DevOpts/whistle/rules/.whistle.dev.js
@@ -5,14 +5,11 @@ module.exports = (cb, util) => {
 
     const ruleList = [
         'now.qq.com/cgi-bin 127.0.0.1:9527',
-        'now.qq.com 127.0.0.1:3000',
-        'www.qq.com 127.0.0.1:3006'
+        'now.qq.com 127.0.0.1:3000'
     ];
-
-    const rules = ruleList.join('\n');
 
     cb({
         name: name,
-        rules: rules
+        rules: ruleList.join('\n')
     });
 };
