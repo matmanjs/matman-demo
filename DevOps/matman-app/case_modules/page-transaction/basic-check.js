cbbed28a1dd579ca0be2ef86da86ad4a35d71c30
linjianghe <linjianghe@tencent.com>
2020-05-05 13:14:14 +0800

    feat: 调试 page-transaction

---
+++ DevOps/matman-app/case_modules/page-transaction/basic-check.js
@@ -0,0 +1,27 @@
+const env = require('./env');
+
+function getResult(opts) {
+    return env.createPageDriver(__filename, opts)
+
+        // 加载页面地址
+        .goto(env.getPageUrl())
+
+        // 需要等待某些条件达成，才开始运行爬虫脚本
+        .wait(env.WAIT.READY)
+
+        // 爬虫脚本的函数，用于获取页面中的数据
+        .evaluate('./crawlers/get-page-info.js')
+
+        // 结束，获取结果
+        .end();
+}
+
+module.exports = getResult;
+
+// getResult({ show: true, doNotCloseBrowser: true, useRecorder: false })
+//     .then(function (result) {
+//         console.log(JSON.stringify(result));
+//     })
+//     .catch(function (error) {
+//         console.error('failed:', error);
+//     });
--- DevOps/matman-app/helpers/index.js
+++ DevOps/matman-app/helpers/index.js
@@ -17,6 +17,9 @@ function createPageDriver(caseModuleFilePath, opts) {
         // 无头浏览器使用 nightmare.js 框架提供，其底层用的是 Google 的 electron，基于 chromium 内核
         .useNightmare({ show: opts.show })
 
+        // 设置浏览器参数
+        .setDeviceConfig('mobile')
+
         // 走指定的代理服务，由代理服务配置请求加载本地项目，从而达到同源测试的目的
         .useProxyServer(`127.0.0.1:${process.env.PORT || 8899}`)
 
