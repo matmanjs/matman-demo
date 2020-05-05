dcf9aa9807228621414d9f8a6cbf20aa642a18ce
helinjiang <onehlj@qq.com>
2020-05-05 13:27:10 +0800

    feat: 调试 page-transaction

---
+++ DevOps/matman-app/test/page-transaction/empty-check.test.js
@@ -0,0 +1,44 @@
+const { expect } = require('chai');
+
+const checkPage = require('../../case_modules/page-transaction/basic-check');
+
+describe('transaction 页面：无流水信息检查', function () {
+    this.timeout(30000);
+
+    let resultData;
+
+    before(function () {
+        return checkPage({
+            show: false,
+            doNotEnd: false,
+            useRecorder: false,
+            queryDataMap: {
+                'get_flow': 'success_empty'
+            }
+        })
+            .then(function (result) {
+                // console.log(JSON.stringify(result));
+                resultData = result;
+            });
+    });
+
+    describe('检查基本信息', function () {
+        let data;
+
+        before(function () {
+            data = resultData.data;
+        });
+
+        it('数据快照校验通过', function () {
+            expect(data).to.eql({
+                'transactionListInfo': {
+                    // 'emptyPic': 'http://now.qq.com/img/nopkdata@2x_c3c9fbba.png',
+                    'emptyWording': '暂无流水记录',
+                    'isExist': true,
+                    'list': [],
+                    'total': 0
+                }
+            });
+        });
+    });
+});
