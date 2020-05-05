ada9138173db9368bc4be31f0557f7abea395dcb
helinjiang <onehlj@qq.com>
2020-05-05 14:45:30 +0800

    refactor: 优化自动化测试案例

--- DevOps/matman-app/case_modules/page-withdraw/withdraw-money-basic.js
+++ DevOps/matman-app/case_modules/page-withdraw/withdraw-money-basic.js
@@ -7,29 +7,29 @@ function getResult(opts) {
         .goto(env.getPageUrl())
 
         // 第一步：开始操作之前
-        .addAction('init', function (nightmareRun) {
-            // nightmareRun 支持所有的原始 nightmare 语法和对其定制的扩展功能
-            return nightmareRun.wait(500);
+        .addAction('init', function (nightmare) {
+            // nightmare 支持所有的原始 nightmare 语法和对其定制的扩展功能
+            return nightmare.wait(500);
         })
 
         // 第二步：选中【5元】
-        .addAction('selectQuota', function (nightmareRun) {
-            return nightmareRun.click('#root .display-withdraw .display-withdraw-quotas .selection .i0');
+        .addAction('selectQuota', function (nightmare) {
+            return nightmare.click('#root .display-withdraw .display-withdraw-quotas .selection .i0');
         })
 
         // 第三步：点击【确定】按钮
-        .addAction('clickSubmit', function (nightmareRun) {
-            return nightmareRun.click('#root .display-withdraw .withdraw-submit .now-button').wait(500);
+        .addAction('clickSubmit', function (nightmare) {
+            return nightmare.click('#root .display-withdraw .withdraw-submit .now-button').wait(500);
         })
 
         // 第四步：点击弹窗中的【确定】按钮
-        .addAction('clickDlgOk', function (nightmareRun) {
-            return nightmareRun.click('.base-alert .dialog-inner .dialog-buttons .dialog-btn.ok');
+        .addAction('clickDlgOk', function (nightmare) {
+            return nightmare.click('.base-alert .dialog-inner .dialog-buttons .dialog-btn.ok');
         })
 
         // 第五步：一秒后再次获取页面状态
-        .addAction('lastCheck', function (nightmareRun) {
-            return nightmareRun.wait(1000);
+        .addAction('lastCheck', function (nightmare) {
+            return nightmare.wait(1000);
         })
 
         // 需要等待某些条件达成，才开始运行爬虫脚本
