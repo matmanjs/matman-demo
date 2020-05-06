5c015cbccffc1fdc22a0bf113617e34bd6be5fbc
helinjiang <onehlj@qq.com>
2020-05-07 07:58:39 +0800

    feat: 新增测试用例

---
+++ test/unit-test/some.test.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+describe('单元测试示例', function () {
+    it('检查字符串匹配', function () {
+        expect('a').to.equal('a');
+    });
+});
---
+++ test/unit-test/subdir/subdir.test.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+describe('单元测试示例2-子文件夹', function () {
+    it('检查字符串匹配2', function () {
+        expect('a2').to.equal('a2');
+    });
+});
