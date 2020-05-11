fceceadace8f833b9fbee1aac0100d6e341d0164
linjianghe <linjianghe@tencent.com>
2020-05-11 08:00:51 +0800

    feat: 修改 config-dev

--- DevOps/devops-app/config-dev.js
+++ DevOps/devops-app/config-dev.js
@@ -1,35 +1,72 @@
 const path = require('path');
+const _ = require('lodash');
 
-const ivwebDevopsWebTest = require('@tencent/dwt-ivweb');
+const {
+    getPluginProject,
+    getPluginUnitTest,
+    getPluginMockstar,
+    getPluginWhistle,
+    getPluginE2ETest,
+    getPluginArchive,
+    getActionConfigByDWTMode
+} = require('./pipelines');
 
-function getTestConfig(opts) {
-    const common = ivwebDevopsWebTest.getTestConfig(__dirname, {
-        shouldRunE2ETest: true,
-        shouldRunUnitTest: true,
-        getWhistleRules: function (testRecord, opts) {
-            const whistleSetting = require(path.join(__dirname, '../whistle'));
+/**
+ * 获得自动化测试的配置
+ *
+ * @param {Object} [opts] 额外选项
+ * @param {Boolean} [opts.shouldRunE2ETest] 是否执行端对端测试，默认值为 true
+ * @param {Boolean} [opts.shouldRunUnitTest] 是否执行单元你测试，默认值为 true
+ * @param {Function} [opts.getWhistleRules] 获得 whistle 的配置，接受两个参数 testRecord 和 opts<projectRootPath,shouldUseMockstar,mockstarPort,name>
+ * @param {Object} [opts.customPluginParams] 插件的自定义配置，是一个 map ，key 值为 插件名字， value 为插件的自定义配置
+ * @return {Object}
+ */
+function getTestConfig(opts = {}) {
+    const { shouldRunUnitTest, shouldRunE2ETest } = getActionConfigByDWTMode(process.env.DWT_MODE);
 
-            return whistleSetting.getDevRules({
-                projectRootPath: opts.projectRootPath,
-                shouldUseMockstar: opts.shouldUseMockstar,
-                mockstarPort: opts.mockstarPort,
-                name: opts.name
-            });
-        },
-        customPluginParams: {
-            project: {
+    const config = {
+        dwtPath: __dirname,
+        plugins: [
+            // 业务项目
+            getPluginProject(shouldRunE2ETest, {
                 usePort: true,
-                buildCmd: function (port, testRecord) {
+                buildCmd: function (testRecord, port) {
                     return port ? `npx cross-env PORT=${port} npm start` : 'npm start';
                 },
                 buildCompleteCheck: function (data) {
                     return data && data.indexOf('Compiled successfully') > -1;
                 }
-            }
-        }
-    });
+            }),
 
-    return Object.assign(common, opts);
+            // 单元测试
+            getPluginUnitTest(shouldRunUnitTest),
+
+            // 数据 mock
+            getPluginMockstar(shouldRunE2ETest),
+
+            // 代理配置
+            getPluginWhistle(shouldRunE2ETest, {
+                getWhistleRules: function (testRecord) {
+                    const whistleSetting = require(path.join(__dirname, '../whistle'));
+
+                    return whistleSetting.getDevRules({
+                        projectRootPath: testRecord.getPlugin('project').rootPath,
+                        shouldUseMockstar: true,
+                        mockstarPort: testRecord.getPlugin('mockstar').port,
+                        name: testRecord.getPlugin('whistle')._processKey
+                    });
+                }
+            }),
+
+            // 端对端测试
+            getPluginE2ETest(shouldRunE2ETest),
+
+            // 归档
+            getPluginArchive()
+        ]
+    };
+
+    return _.merge(config, opts);
 }
 
 module.exports = {
