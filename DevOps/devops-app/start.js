2ee1e321fd4b68f6c6001292ebae8ead0de2f3bf
helinjiang <onehlj@qq.com>
2020-05-12 12:26:55 +0800

    feat: 将测试过程的一些数据返回

--- DevOps/devops-app/start.js
+++ DevOps/devops-app/start.js
@@ -55,7 +55,8 @@ async function run() {
 
     try {
         // 项目 install
-        await dwt.runByExec('npm install', { cwd: testRecord.project.rootPath });
+        testRecord.project.installCmd = 'npm install';
+        await dwt.runByExec(testRecord.project.installCmd, { cwd: testRecord.project.rootPath });
 
         // 需要安装自动化测试需要的 reporter 等组件的依赖
         await dwt.runByExec('npm install cross-env nyc mocha-multi-reporters mochawesome mocha-junit-reporter --no-save', { cwd: testRecord.project.rootPath });
@@ -64,28 +65,25 @@ async function run() {
         const unitTestReporterConfigFile = await generateMochaReporterConfigFile(dwt, testRecord.unitTest.outputPath);
 
         // 执行单元测试
-        testRecord.unitTest.testCmdToExecute = `npx cross-env BABEL_ENV=test mocha test/unit --reporter mocha-multi-reporters --reporter-options configFile=${unitTestReporterConfigFile}`;
-        await dwt.runByExec(testRecord.unitTest.testCmdToExecute, { cwd: testRecord.unitTest.runTestPath });
+        testRecord.unitTest.testCmd = `npx cross-env BABEL_ENV=test mocha test/unit --reporter mocha-multi-reporters --reporter-options configFile=${unitTestReporterConfigFile}`;
+        await dwt.runByExec(testRecord.unitTest.testCmd, { cwd: testRecord.unitTest.runTestPath });
 
         // 执行单元测试覆盖率
-        await dwt.runByExec(() => {
-            const { testCmdToExecute, coverageOutputPath } = testRecord.unitTest;
-
-            const cmdArr = [
-                `npx nyc --silent ${testCmdToExecute}`,
-                `npx nyc report --reporter=html --report-dir=${coverageOutputPath}`,
-                `npx nyc report --reporter=cobertura --report-dir=${coverageOutputPath}`,
-                `npx nyc report --reporter=lcovonly --report-dir=${coverageOutputPath}`
-            ];
-
-            return cmdArr.join(' && ');
-        }, { cwd: testRecord.project.rootPath });
+        testRecord.unitTest.coverageCmd = [
+            `npx nyc --silent ${testRecord.unitTest.testCmd}`,
+            `npx nyc report --reporter=html --report-dir=${testRecord.unitTest.coverageOutputPath}`,
+            `npx nyc report --reporter=cobertura --report-dir=${testRecord.unitTest.coverageOutputPath}`,
+            `npx nyc report --reporter=lcovonly --report-dir=${testRecord.unitTest.coverageOutputPath}`
+        ].join(' && ');
+        await dwt.runByExec(testRecord.unitTest.coverageCmd, { cwd: testRecord.project.rootPath });
 
         // 构建项目
-        await dwt.runByExec('npx cross-env ENABLE_E2E_TEST=1 npm run build', { cwd: testRecord.project.rootPath });
+        testRecord.project.buildCmd = 'npx cross-env ENABLE_E2E_TEST=1 npm run build';
+        await dwt.runByExec(testRecord.project.buildCmd, { cwd: testRecord.project.rootPath });
 
         // mockstar-app 安装依赖
-        await dwt.runByExec('npm install', { cwd: testRecord.mockstar.rootPath });
+        testRecord.mockstar.installCmd = 'npm install';
+        await dwt.runByExec(testRecord.mockstar.installCmd, { cwd: testRecord.mockstar.rootPath });
 
         // 为 mockstar 获得一个没有被占用的端口
         const mockstarPort = await dwt.findAvailablePort('mockstar');
@@ -96,26 +94,33 @@ async function run() {
             return data && data.indexOf(`127.0.0.1:${mockstarPort}`) > -1;
         });
 
+        testRecord.mockstar.startPid = mockstarStartCmd.pid;
+
         // 为 mockstar 锁定这个已被占用的端口
-        await dwt.lockPort('mockstar', mockstarPort, mockstarStartCmd.pid);
+        await dwt.lockPort('mockstar', mockstarPort, testRecord.mockstar.startPid);
 
         // 为 whistle 获得一个没有被占用的端口
         const whistlePort = await dwt.findAvailablePort('whistle');
         testRecord.whistle.port = whistlePort;
 
         // whistle 启动
-        const whistleStartCmd = await dwt.runByExec(`w2 start -S ${testRecord.whistle.namespace} -p ${whistlePort}`, {}, (data) => {
+        testRecord.whistle.startCmd = `w2 start -S ${testRecord.whistle.namespace} -p ${whistlePort}`;
+        const whistleStartCmd = await dwt.runByExec(testRecord.whistle.startCmd, {}, (data) => {
             return data && data.indexOf(`127.0.0.1:${whistlePort}`) > -1;
         });
 
+        testRecord.whistle.startPid = whistleStartCmd.pid;
+
         // 为 whistle 锁定这个已被占用的端口
-        await dwt.lockPort('whistle', whistlePort, whistleStartCmd.pid);
+        await dwt.lockPort('whistle', whistlePort, testRecord.whistle.startPid);
 
         // 检查 whistle 是否实际ok
         await dwt.checkIfWhistleIsStarted(whistlePort);
 
         // 产生 whistle 规则配置文件
         const whistleRulesConfigFile = path.join(dwt.outputPath, 'test.whistle.js');
+        testRecord.whistle.whistleRulesConfigFile = whistleRulesConfigFile;
+
         await dwt.generateWhistleRulesConfigFile(whistleRulesConfigFile, () => {
             const whistleSetting = require(path.join(__dirname, '../whistle'));
 
@@ -128,19 +133,23 @@ async function run() {
         });
 
         // 使用这个 whistle 规则文件
-        await dwt.runByExec(`w2 use ${whistleRulesConfigFile} -S ${testRecord.whistle.namespace} --force`);
+        testRecord.whistle.useCmd = `w2 use ${whistleRulesConfigFile} -S ${testRecord.whistle.namespace} --force`;
+        await dwt.runByExec(testRecord.whistle.useCmd);
 
         // matman-app 安装依赖
-        await dwt.runByExec('npm install', { cwd: testRecord.matman.rootPath });
+        testRecord.matman.installCmd = `npm install`;
+        await dwt.runByExec(testRecord.matman.installCmd, { cwd: testRecord.matman.rootPath });
 
         // matman-app 构建
-        await dwt.runByExec('npm run build', { cwd: testRecord.matman.rootPath });
+        testRecord.matman.buildCmd = `npm run build`;
+        await dwt.runByExec(testRecord.matman.buildCmd, { cwd: testRecord.matman.rootPath });
 
         // 为端对端测试配置生成 reporter 配置
         const e2eTestReporterConfigFile = await generateMochaReporterConfigFile(dwt, testRecord.e2eTest.outputPath);
 
         // 执行端对端测试
-        await dwt.runByExec(`npx cross-env WHISTLE_PORT=${whistlePort} mocha test/e2e --reporter mocha-multi-reporters --reporter-options configFile=${e2eTestReporterConfigFile}`, { cwd: testRecord.e2eTest.runTestPath });
+        testRecord.e2eTest.testCmd = `npx cross-env WHISTLE_PORT=${whistlePort} mocha test/e2e --reporter mocha-multi-reporters --reporter-options configFile=${e2eTestReporterConfigFile}`;
+        await dwt.runByExec(testRecord.e2eTest.testCmd, { cwd: testRecord.e2eTest.runTestPath });
 
     } catch (err) {
         console.error('run catch err', err);
