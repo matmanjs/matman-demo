d0179bcbaecd44b4560b2633fa33becc8277eec5
helinjiang <onehlj@qq.com>
2020-09-03 15:52:20 +0800

    refactor: 优化 e2e test 体验

--- test/bootstrap-sut.js
+++ test/bootstrap-sut.js
@@ -1,55 +1,23 @@
-const path = require('path');
-const { E2ERunner } = require('matman-e2e-test');
-const whistle = require('../DevOps/whistle');
+const { createE2ERunner, prepareSUT } = require('./e2e.config');
 
 const bootstrapMockstarPort = 9527 || process.env.MOCKSTAR_PORT;
 const bootstrapWhistlePort = 8899 || process.env.WHISTLE_PORT;
 
 (async () => {
-  const WORKSPACE_PATH = path.join(__dirname, '../');
-  const OUTPUT_PATH = path.join(__dirname, '../.matman_output');
+  // 创建 E2ERunner
+  const e2eRunner = await createE2ERunner();
 
-  const e2eRunner = new E2ERunner({
-    workspacePath: WORKSPACE_PATH,
-    outputPath: OUTPUT_PATH,
-  });
-
-  // 开始启动
+  // 设置启动
   await e2eRunner.start();
 
-  // 构建项目
-  await e2eRunner.buildProject('npx cross-env ENABLE_E2E_TEST=1 npm run build', {
-    cwd: WORKSPACE_PATH,
-  });
-
-  // 启动 mockstar
-  const mockstarAppPath = path.join(WORKSPACE_PATH, './DevOps/mockstar-app');
-  const mockstarPort = await e2eRunner.startMockstar(mockstarAppPath, {
-    port: bootstrapMockstarPort,
-  });
-
-  // 启动 whistle
-  const whistlePort = await e2eRunner.startWhistle({
-    port: bootstrapWhistlePort,
+  // 测试之前准备环境
+  const prepareSUTResult = await prepareSUT(e2eRunner, {
+    mockstarPort: bootstrapMockstarPort,
+    whistlePort: bootstrapWhistlePort,
     useCurrentStartedWhistle: true,
-    getWhistleRules: () => {
-      return whistle.getProdRules({
-        projectRootPath: WORKSPACE_PATH,
-        shouldUseMockstar: true,
-        mockstarPort,
-      });
-    },
   });
-
-  // 启动 matman
-  const matmanAppPath = path.join(WORKSPACE_PATH, './DevOps/matman-app');
-  await e2eRunner.startMatman(matmanAppPath);
 
   // debug 日志
-  console.log({
-    bootstrapMockstarPort,
-    bootstrapWhistlePort,
-    mockstarPort,
-    whistlePort,
-  });
+  console.log(prepareSUTResult);
+  console.log(e2eRunner);
 })();
